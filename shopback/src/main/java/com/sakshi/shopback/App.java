92c9a15c6b2b4a6f3c3f59f03a7907ba948fbf6a
Sakshi Srivastava <sakshisrivastav712@gmail.com>
2018-12-04 12:28:33 +0530

    My Project setup completed successfully

---
+++ shopback/src/main/java/com/sakshi/shopback/App.java
@@ -0,0 +1,13 @@
+package com.sakshi.shopback;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
